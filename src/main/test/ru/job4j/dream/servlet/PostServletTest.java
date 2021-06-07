10653e57440ca1723cd08e9a93c37beb164561f5
EvgeniyDanisevich <a937va@gmail.com>
2021-06-07 20:26:22 +0300

    1. Написать тест на PostServlet. [#2518 #165298]

--- src/main/test/ru/job4j/dream/servlet/PostServletTest.java
+++ src/main/test/ru/job4j/dream/servlet/PostServletTest.java
@@ -11,12 +11,15 @@ import ru.job4j.dream.store.MemPostStore;
 import ru.job4j.dream.store.PsqlPostStore;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(PsqlPostStore.class)
@@ -36,5 +39,27 @@ public class PostServletTest {
         Post result = store.findAll().iterator().next();
         Assert.assertEquals(result.getName(), "Post Name");
         Assert.assertEquals(result.getDescription(), "Post Description");
+    }
+
+    @Test
+    public void whenDoGet() throws IOException, ServletException {
+        Store<Post> store = MemPostStore.instOf();
+        PowerMockito.mockStatic(PsqlPostStore.class);
+        PowerMockito.when(PsqlPostStore.instOf()).thenReturn(store);
+        PostServlet postServlet = new PostServlet();
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        HttpSession session = mock(HttpSession.class);
+        String address = "posts.jsp";
+        String user = "user";
+        when(req.getRequestDispatcher(address)).thenReturn(dispatcher);
+        when(req.getSession()).thenReturn(session);
+        when(session.getAttribute("user")).thenReturn(user);
+        postServlet.doGet(req, resp);
+        verify(req).getRequestDispatcher(address);
+        verify(req).setAttribute("posts", store.findAll());
+        verify(req).setAttribute("user", session.getAttribute("user"));
+        verify(dispatcher).forward(req, resp);
     }
 }
